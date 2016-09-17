b0511d1ec3ff5ff5893cfc7d490ac6ff2a3d7988
austin <austinswchang@gmail.com>
2016-09-17 12:13:14 -0700

    got the backwards down

--- pallindrome.cpp
+++ pallindrome.cpp
@@ -4,12 +4,29 @@
 using namespace std;
 
 int main(){
-  char str[41]; //gives size (the part in the brackets)
+  char str[81]; //gives size (the part in the brackets)
   cout << "Give me a word or phrase with at most 40 characters" << endl;
-  cin.get(str, 41); //keeps reading until 20 characters are up
-  //a comment to see what austins messing up
-  cout << str << endl;
+  cin.get(str, 81); //keeps reading until 80 characters are up
+  cin.get();
+  char str2[81];
   
-  //if (strcpy(str,str2) ==0)
+  int count = 0;
+  //if (strcpy(str,str2) ==0){}
+  for (int i = 81; i > -1; i--){
+    if (str[i] == '\0'){
+      str2[count] = '\0';
+    }
+   if (str[i] != '\0'){
+      str2[count] = str[i];
+      count++;
+    }
+   
+  }
+ 
+  cout << str << endl;
+  cout << str2 << endl;
+  if (strcmp(str, str2) == 0){
+    cout << "they are equal" << endl;
+  }
   return 0;
 }
