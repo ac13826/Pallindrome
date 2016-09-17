3184ac964a8520b135f7bc73def7b22dc6681d68
austin <austinswchang@gmail.com>
2016-09-17 13:43:47 -0700

    almost done

--- pallindrome.cpp
+++ pallindrome.cpp
@@ -4,29 +4,45 @@
 using namespace std;
 
 int main(){
-  char str[81]; //gives size (the part in the brackets)
+
+  char strog[81]; //gives size (the part in the brackets)
   cout << "Give me a word or phrase with at most 40 characters" << endl;
-  cin.get(str, 81); //keeps reading until 80 characters are up
+  cin.get(strog, 81); //keeps reading until 80 characters are up
   cin.get();
   char str2[81];
+  char str[81];
   
   int count = 0;
-  //if (strcpy(str,str2) ==0){}
-  for (int i = 81; i > -1; i--){
-    if (str[i] == '\0'){
-      str2[count] = '\0';
+  int together = 0;
+  
+  for (int k = 0; k < 80; k++){
+    if(strog[k] != ' ' && (ispunct(strog[k]) == false)){
+      str[together] = tolower(strog[k]);
+      together++;
     }
+  }
+
+  // make compare
+  for (int i = 80; i > -1; i--){
+    if (str[i] == '\0'){
+      str2[count] = str[i];
+      count = 0;
+      }
    if (str[i] != '\0'){
       str2[count] = str[i];
       count++;
     }
+   str2[count] = '\0';
    
   }
- 
+
   cout << str << endl;
   cout << str2 << endl;
   if (strcmp(str, str2) == 0){
     cout << "they are equal" << endl;
+  }
+  else {
+    cout << "not a pallindrome" << endl;
   }
   return 0;
 }
