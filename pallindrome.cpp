61523be915586012055649c4223a6f213e40fa65
austin <austinswchang@gmail.com>
2016-09-19 13:29:04 -0700

    Final finish

--- pallindrome.cpp
+++ pallindrome.cpp
@@ -6,15 +6,15 @@ using namespace std;
 int main(){
 
   char strog[81]; //gives size (the part in the brackets)
-  cout << "Give me a word or phrase with at most 40 characters" << endl;
+  cout << "Give me a word or phrase with at most 80 characters" << endl;
   cin.get(strog, 81); //keeps reading until 80 characters are up
   cin.get();
   char str2[81];
   char str[81];
-  
+  char yesno;
   int count = 0;
   int together = 0;
-  
+ 
   for (int k = 0; k < 80; k++){
     if(strog[k] != ' ' && (ispunct(strog[k]) == false)){
       str[together] = tolower(strog[k]);
@@ -39,7 +39,7 @@ int main(){
   cout << str << endl;
   cout << str2 << endl;
   if (strcmp(str, str2) == 0){
-    cout << "they are equal" << endl;
+    cout << "Your word, " << strog << " was a pallindrome" << endl;
   }
   else {
     cout << "not a pallindrome" << endl;
